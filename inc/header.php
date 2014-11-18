68d77ee70f89f3756aabaa6b4342d8fff412b341
AlexSchieving <alexandre.schieving@isep.fr>
2014-11-18 21:08:45 +0100

    Intégration du slider dans le header
    Tout le slider est compris le fichier header.php

--- inc/header.php
+++ inc/header.php
@@ -6,36 +6,8 @@
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<title>Sweet Home Exchange : échangez vos propriétés en toute sûreté !</title>
-		<script type="text/javascript">
-		function slideSwitch() {
-    var $active = $('#slideshow IMG.active');
-  
-    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
-  
-    // use this to pull the images in the order they appear in the markup
-    var $next =  $active.next().length ? $active.next()
-        : $('#slideshow IMG:first');
-  
-    // uncomment the 3 lines below to pull the images in random order
-  
-    // var $sibs  = $active.siblings();
-    // var rndNum = Math.floor(Math.random() * $sibs.length );
-    // var $next  = $( $sibs[ rndNum ] );
-  
-    $active.addClass('last-active');
-  
-    $next.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            $active.removeClass('active last-active');
-        });
-}
-  
-$(function() {
-    setInterval( "slideSwitch()", 5000 );
-});
-</script>
-<script type="text/javascript" src="js/jquery.montage.min.js"></script>
+		<script type="text/javascript" src="js/slider.js"></script>
+        <script type="text/javascript" src="js/jquery.montage.min.js"></script>
 	</head>
 	<body>
   
@@ -46,9 +18,15 @@ $(function() {
 			<li class="bouton_1"><a href="index.php">Accueil</a></li>
 			<li class="bouton_1"><a href="offres.php">Voir les offres</a></li>
 			<li class= "bouton_1"><a href="acc_forum.php">Forum</a></li>
-			<li class="bouton_1"><a href="#">Modifier son profil</a>
-			<li class="bouton_1"><a href="#">Se déconnecter</a></li>
+			<!--<li class="bouton_1"><a href="#">Modifier son profil</a>
+			<li class="bouton_1"><a href="#">Se déconnecter</a></li> -->
     </ul>
     <div style="clear:both;">
     </div>
  </div>
+ <div id="slideshow">
+    <img class="active" src="http://www.hiphoprepublican.com/wp-content/uploads/2013/06/New-York-City-Sun.jpg" alt="Slideshow Image 1" />
+    <img src="img/image1.jpg" alt="Slideshow Image 2" />
+    <img src="img/image2.jpg" alt="Slideshow Image 3" />
+    <img src="img/image3.jpg" alt="Slideshow Image 4" />
+</div>
