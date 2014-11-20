b264fe37061c267437e1f0850dbe8f087c5c3fb0
benazzara <bazzara23@gmail.com>
2014-11-20 11:31:26 +0100

    Create menu.php
    necessaire pour le "include" dans contact et historique

---
+++ menu.php
@@ -0,0 +1,15 @@
+<header>
+		<ul id="header">
+			<a href="index.php"><img src="image/logo_SHE.png"/></a>
+		</ul>
+			<ul id="menu1">
+				<li class="bouton_1"><a href="index.php">Accueil</a></li>
+				<li class="bouton_1"><a href="offres.php">Voir les offres</a></li>
+				<li class= "bouton_1"><a href="acc_forum.php">Forum</a></li>
+			</ul>
+		
+		<div id="intro">
+			<?php echo "Bienvenue sur S.H.E. !" ?>
+		</div>
+
+</header>
