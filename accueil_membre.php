eca3fdc88f2ae6d156104736c9ab089123ac8c62
EtienneBouvier <et.bouvier@wanadoo.fr>
2014-11-27 19:06:21 +0100

    include accueil membre et non membre

---
+++ accueil_membre.php
@@ -0,0 +1,8 @@
+<?php include('inc/slider.php');?>
+<?php include('inc/header.php');?>
+<div class="barre_recherche">
+    <input type="text" placeholder="Rechercher une Annonce" name="recherche" class="recherche"/>
+    <input tpe="submit" value="Rechercher" class="Rechercher"/>
+</div>
+
+ <a class="lien_recherche" href="#">Recherche avancée</a>	
