250d58e915eac3e4af765c84720a55670c619baf
AlexSchieving <alexandre.schieving@isep.fr>
2014-12-04 11:48:29 +0100

    page index et register.php + Css

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+$membre = 0;
 function error ($nom) {
   global $errors;
   echo (isset($errors[$nom]) ? $errors[$nom] : '');
@@ -35,9 +35,7 @@ if(!empty($_POST))
 {
   // Vérification des données entrées par l'utilisateur
   verif("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", 'Email', 'email');
-  error('email');
   verif("#^.+$#", 'Mot de passe', 'password');
-  error('password');
     if(empty($errors))
       {
         // Cryptage du mot de passe
@@ -47,28 +45,50 @@ if(!empty($_POST))
             $db = new PDO("mysql:host=localhost;dbname=mydb", "root", "root");
             // Permet d'afficher les erreurs envoyées par SQL
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          } catch (PDOException $e) {
+          } catch (Exception $e) {
             die("Erreur:". $e->getMessage());
           }
         // Génération de la requête SQL
-        $sql='SELECT id_user FROM user WHERE email = :email and password = :password';
+        try {
+          $req =$db->prepare('SELECT * FROM user WHERE email = :email AND password = :password');
+          $req->execute($form);
+        } catch (Exception $e) {
+          die ("Erreur :" .$e->getMessage());
+        }
         // On essaie d'exécuter la requête
-        $req = $db->query($sql);
         $res = $req->fetch();
-          if ($res) {
+          //echo '<pre>';
+          //print_r($res);
+          //echo '</pre>';
+          if (isset($res) && (!empty($res))) {
           $errors['global'] = message('Vous êtes connecté.');
+          session_start();
+          $_SESSION['id'] = $res[0];
+          $_SESSION['prenom'] = $res[1];
+          $_SESSION['nom'] = $res[2]; 
+          $_SESSION['city'] = $res[3];
+          $_SESSION['pays'] = $res[4];
+          $_SESSION['n_port'] = $res[5];
+          $_SESSION['email'] = $res[6];
+          $_SESSION['code_p'] = $res[7];
+          $_SESSION['n_fixe'] = $res[8];
+          $_SESSION['date_naiss'] = $res[9];
+          $_SESSION['date_insc'] = $res[10];
+          $_SESSION['gender'] = $res[11];
+          $_SESSION['address'] = $res[13];
+          $_SESSION['region'] = $res[14];
+          $membre = 1;
           }   
-        else {
-              message('Mauvais identifiant ou mot de passe.','error');
+          elseif(isset($res) && (empty($res))){
+              $errors['login'] = message('Mauvais identifiant ou mot de passe.','error');
           }  
       }    
-}      
-
-include ('inc/header_inv.php');
-include ('inc/slider.php');
-?>
-<div id="barre_connect">
-<form method="post" action="index.php" >
+} 
+if($membre != 1)
+  {
+  include ('inc/header_inv.php');
+  echo '<div id="barre_connect">';
+  echo '<form method="post" action="index.php" >
   <legend>Connectez-vous</legend> 
   <input type="text" name="email" id="email" class="connect1" placeholder="Adresse mail" required/>
   <input type="password" name="password" class="connect2" id="password" placeholder="Mot de Passe" required/>
@@ -77,8 +97,31 @@ include ('inc/slider.php');
 </div>
 <div id="lien_inscr">
   <a href="oublie_mdp.php">Mot de passe oublié ?</a>
-  <a href="inscription.php">S'inscrire</a>
-</div>'
+  <a href="register.php">S\'inscrire</a>';
+echo '</div>';
+} 
+else {
+  include ('inc/header.php');
+  include('inc/esp_membre.php');
+  echo '<div class="barre_recherche">';
+  echo '<form method="post" action="resultats_recherchem.php">
+  <legend>Recherchez une annonce</legend>
+  <input type="text" name="recherche" class="recherche" placeholder="Mot-clé" required />
+  <input type="submit" value="Rechercher" class="rechercher"/>;';
+  echo '</div>';
+  echo '<div class="lien_recherche">
+      <a href="recherchem.php">Recherche avancée</a>';
+  echo '</div>';   
+
+} 
+include ('inc/slider.php');     
+?>
+<?php error('email');
+      error('password'); ?>
+<?php echo isset($errors['login']) ? $errors['login'] : ''; ?>
+<?php echo isset($_SESSION) ? 'Bonjour ' . $_SESSION['nom'] . '  ' . $_SESSION['prenom'] . ' !' : ''; ?>
+
+
 <div id="mosaic">
   <h2 id="titre_mos">Les Mieux Notés</h2>
 <table id="mos" border="0" callpadding="0" cellspacing="0">
@@ -101,5 +144,6 @@ include ('inc/slider.php');
     <td><a href="#"><img id="mos_img" src="img/Maison5.jpg" width="500px" height="300px"></a></td>
   </tr>
 </table>
+</div>
 </div>
 <?php include('inc/footer.php'); ?>
